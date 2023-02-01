306f1f347a8160a2b69bc5a41fd6401ddaece750
Ilya Zhigarev <zhigarev@bk.ru>
2023-02-01 19:19:06 +0100

    feat: add start pack express + sequelize

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 3000;
+import db from './models';
+
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+      console.log(`Magic happening on port ${port}`)
+    })
+  }
+)
