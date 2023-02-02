beab3e7f05158b12df4e4a83b57f68c8f722bdfb
Ilya Zhigarev <zhigarev@bk.ru>
2023-02-02 22:23:55 +0100

    feat: add redis

--- index.ts
+++ index.ts
@@ -3,18 +3,38 @@ const cors = require("cors");
 require('dotenv').config()
 
 const app = express()
+const session = require("express-session");
+const redis = require("redis");
+const RedisStore = require("connect-redis")(session);
+const redisClient = redis.createClient({url: process.env.REDIS_URL});
 const port = process.env.PORT || 3000;
+const {COOKIE_SECRET, COOKIE_NAME} = process.env;
 const authRouter = require('./routes/auth.router')
 app.use(express.json());
+app.set("cookieName", COOKIE_NAME);
 app.use(cors(
   {
-    // origin: ['http://localhost:3000'],
-    origin: ['https://json-statham.com'],
-    // origin: ['https://json-statham.com', 'http://localhost:3000'],
-    // origin: '*',
+    origin: ['https://json-statham.com', 'http://localhost:3000'],
     credentials: true
   }
 ));
+app.use(
+  session({
+    name: app.get("cookieName"),
+    secret: COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new RedisStore(
+      {client: redisClient},
+      {secret: COOKIE_SECRET}
+    ),
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1e3 * 60*60, // COOKIE'S LIFETIME — 1 DAY
+    },
+  })
+);
 
 // import db from './models';
 // import { users } from './seeders/users';
